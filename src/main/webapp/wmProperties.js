f61a977ee77981871ee82becad3a9aa9d456e7d6
root <root@dfe4f77da01e>
2017-06-23 00:47:10 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_JoZUHTEcaT",
+  "homePage" : "Main",
+  "name" : "Auto_JoZUHTEcaT",
+  "platformType" : "DEFAULT",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.0"
+};
